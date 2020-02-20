aa5705fd5330d6056b5595ab09078fe21e5b7d7e
Daniel Ortez <daniortez882@gmail.com>
2020-02-20 17:29:24 +0100

    router logic added v1.0.0

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+import Header from '../templates/Header'
+import Home from '../pages/Home'
+import Character from '../pages/Character'
+import Error404 from '../pages/404'
+
+import getHash from '../utils/getHash'
+import resolveRoutes from '../utils/resolveRoutes'
+
+const routes = {
+  '/': Home(),
+  '/:id': Character(),
+  '*': Error404()
+}
+
+const router = async () => {
+  const header = document.getElementById('header') || null
+  const content = document.getElementById('content') || null
+
+  header.innerHTML = await Header()
+
+  const hash = getHash()
+  const route = await resolveRoutes(hash)
+  const render = routes[route] ? routes[route] : Error404
+
+  content.innerHTML = await render
+
+  return {
+    header,
+    content
+  }
+}
+
+export default router
---
+++ src/templates/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  const view = `
+    <div class="header-main">
+      <div class="header-logo">
+        <h1> <a href="#/">100ti.co</a> </h1>
+      </div>
+      <div class="header-nav">
+        <a href="#/about/">About</a>
+      </div>
+    </div>
+  `
+
+  return view
+}
+
+export default Header
